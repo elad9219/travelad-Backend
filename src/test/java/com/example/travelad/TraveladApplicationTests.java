47c902a0618305d6cc55e835e8fbf1f914ae062f
elad9219 <elad9219@gmail.com>
2024-09-30 15:38:56 +0300

    SwaggerConfig added

---
+++ src/test/java/com/example/travelad/TraveladApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
